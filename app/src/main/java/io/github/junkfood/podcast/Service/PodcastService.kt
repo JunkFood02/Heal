aab40164c864c4c0d3135e238f8fd4283a270cf3
hmc <3241126481@qq.com>
2022-06-22 14:56:02 +0800

    test

---
+++ app/src/main/java/io/github/junkfood/podcast/Service/PodcastService.kt
@@ -0,0 +1,13 @@
+package io.github.junkfood.podcast.Service
+
+import android.app.Service
+import android.content.Intent
+import android.os.IBinder
+
+class PodcastService : Service() {
+
+    override fun onBind(intent: Intent): IBinder {
+        TODO("Return the communication channel to the service.")
+
+    }
+}
